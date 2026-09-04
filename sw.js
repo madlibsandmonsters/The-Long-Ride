13a37c2607ec739c197c2f571526fd47e63a7d44
madlibsandmonsters <madlibsandmonsters@gmail.com>
2026-09-04 18:09:24 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE="longride-v1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));
+});
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const cp=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,cp).catch(()=>{})); return res;
+  }).catch(()=>caches.match("./index.html"))));
+});
