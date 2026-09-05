893bb46b95f902861a1f1bff301195ab8e6e44a9
madlibsandmonsters <madlibsandmonsters@gmail.com>
2026-09-05 01:06:51 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="longride-v1";
+const CACHE="longride-v3";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{
   self.skipWaiting();
